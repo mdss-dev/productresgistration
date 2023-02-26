171550a5f538e976d9276b70bec158660f37066e
Michael Douglas <douglas.ti.dev@gmail.com>
2023-02-26 16:55:40 -0300

    Classe produto criada

--- src/main/java/com/example/productregistration/entity/Produto.java
+++ src/main/java/com/example/productregistration/entity/Produto.java
@@ -1,2 +1,58 @@
-package com.example.productregistration.entity;public class Produto {
+package com.example.productregistration.entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import javax.annotation.processing.Generated;
+
+public class Produto {
+
+    /*
+    * A anotação @Entity indica que esta classe representa uma entidade JPA.
+    *  Os campos id, nome, preco e quantidade representam os atributos da entidade.
+    A anotação @Id indica que o campo id é a chave primária da entidade.
+    * A anotação @GeneratedValue indica que o valor do campo id será gerado automaticamente
+    * pelo banco de dados.*/
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private Double preco;
+
+    private Integer quantidade;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
 }
--- src/test/java/com/example/productregistration/ProductRegistrationApplicationTests.java
+++ src/test/java/com/example/productregistration/ProductRegistrationApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ProductRegistrationApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
